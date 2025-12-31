453542f554a85efe59591cec09427ff09d2ae1bb
Aaditya Kumar Vishwakarma <aadityakumarvishwakarma31@gmail.com>
2025-12-31 10:31:46 +0530

    Style Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,12 +3,14 @@ import React from 'react'
 
 const Navbar = () => {
     return (
-        <nav className='w-full h-18 flex items-center justify-center'>
+        <nav className='w-full h-18 flex items-center justify-between px-10 lg:px-30'>
             <h1>Welcome</h1>
-            <Link href={"/"}>Home</Link>
-            <Link href={"/yourexpenses"}>Your Expenses</Link>
+            <div className='flex items-center justify-center gap-4 lg:gap-12'>
+                <Link href={"/"}>Home</Link>
+                <Link href={"/yourexpenses"}>Your Expenses</Link>
+            </div>
         </nav>
     )
 }
 
-export default Navbar
+export default Navbar;
